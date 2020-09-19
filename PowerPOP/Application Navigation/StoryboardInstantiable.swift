a7185a49c6a114b6ff2008f8c05d89cefe873772
Ashish Badak <ashbadak@gmail.com>
2020-09-19 14:00:00 +0530

    created StoryboardInstantiable protocol with default implementations

--- PowerPOP/Application Navigation/StoryboardInstantiable.swift
+++ PowerPOP/Application Navigation/StoryboardInstantiable.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StoryboardInstantiable {
+    static var storyboardName: String { get }
+    static var storyboardBundle: Bundle? { get }
+    static var storyboardIdentifier: String { get }
+    
+    static func instantiate() -> Self
+}
+
+extension StoryboardInstantiable where Self: UIViewController {
+    static var storyboardBundle: Bundle? { nil }
+    static var storyboardIdentifier: String { String(describing: self) }
+    
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: storyboardBundle)
+        
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as? Self else {
+            fatalError("Failed to load \(storyboardIdentifier) from \(storyboardName) storyboard.")
+        }
+        
+        return viewController
+    }
+}
