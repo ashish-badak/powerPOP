8b4399c480b42fe7af3cd2067c59a30e2e886db4
Ashish Badak <ashbadak@gmail.com>
2020-09-20 21:24:18 +0530

    created tab bar item styles

--- PowerPOP/Utility/Extensions/UITabBarItemExtension.swift
+++ PowerPOP/Utility/Extensions/UITabBarItemExtension.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITabBarItem {
+    static var houses: Self {
+        .init(
+            title: "Houses",
+            image: UIImage(named: "houses")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "houses-selected")?.withRenderingMode(.alwaysOriginal)
+        )
+    }
+    
+    static var characters: Self {
+        .init(
+            title: "Characters",
+            image: UIImage(named: "characters")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "characters-selected")?.withRenderingMode(.alwaysOriginal)
+        )
+    }
+}
