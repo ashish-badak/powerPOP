f9b243fc8bf7880c0108863c8890b3a4b9a8dfc6
Ashish Badak <ashbadak@gmail.com>
2020-09-20 21:37:41 +0530

    added DashboardTabbarComponent file

---
+++ PowerPOP/Dashboard/Model/DashboardTabbarComponent.swift
@@ -0,0 +1,9 @@
+//
+//  DashboardTabbarComponent.swift
+//  PowerPOP
+//
+//  Created by Ashish Personal on 20/09/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
