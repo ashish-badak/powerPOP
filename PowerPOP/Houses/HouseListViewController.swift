e80f3604ed875e054e0a93e017973376362780a9
Ashish Badak <ashbadak@gmail.com>
2020-09-20 18:45:23 +0530

    added HouseListViewController and CharacterListViewController files and scenes

---
+++ PowerPOP/Houses/HouseListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HouseListViewController.swift
+//  PowerPOP
+//
+//  Created by Ashish Personal on 20/09/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class HouseListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
