84317cc41ca78d9138a5aa6b5ed0929219bc5873
Ashish Badak <ashbadak@gmail.com>
2020-09-26 14:19:49 +0530

    added cell registration functions for collection view cells

--- PowerPOP/Utility/Extensions/UICollectionViewExtension.swift
+++ PowerPOP/Utility/Extensions/UICollectionViewExtension.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func register<Cell: UICollectionViewCell>(_ type: Cell.Type) {
+        self.register(Cell.self, forCellWithReuseIdentifier: Cell.reuseIdentifier)
+    }
+    
+    func register<Cell: UICollectionViewCell>(_ type: Cell.Type) where Cell: NibLoadableView {
+        self.register(Cell.nib, forCellWithReuseIdentifier: Cell.reuseIdentifier)
+    }
+}
