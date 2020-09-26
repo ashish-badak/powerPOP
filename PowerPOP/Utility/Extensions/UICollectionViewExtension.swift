8f250cc840eca1505a033e5b732dd3c5a57eadec
Ashish Badak <ashbadak@gmail.com>
2020-09-26 14:03:09 +0530

    added UICollectionViewExtension and UITableViewExtension files

---
+++ PowerPOP/Utility/Extensions/UICollectionViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionViewExtension.swift
+//  PowerPOP
+//
+//  Created by Ashish Personal on 26/09/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
