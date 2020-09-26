35cf6349af5a204e3aee4246861a95db9efd2ca7
Ashish Badak <ashbadak@gmail.com>
2020-09-26 14:16:01 +0530

    added function to dequeue generic table view cell

--- PowerPOP/Utility/Extensions/UITableViewExtension.swift
+++ PowerPOP/Utility/Extensions/UITableViewExtension.swift
@@ -16,4 +16,13 @@ extension UITableView {
     func register<Cell: UITableViewCell>(_ type: Cell.Type) where Cell: NibLoadableView {
         self.register(Cell.nib, forCellReuseIdentifier: Cell.reuseIdentifier)
     }
+    
+    func dequeue<Cell: UITableViewCell>(forIndexPath indexPath: IndexPath) -> Cell {
+        guard let cell = self.dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath) as? Cell
+            else {
+                fatalError("Failed to dequeue cell with reuseIdentifier: \(Cell.reuseIdentifier)")
+        }
+        
+        return cell
+    }
 }
