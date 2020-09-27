c8d6bdec2faad08f91fa5623846488d84d43a400
Ashish Badak <ashbadak@gmail.com>
2020-09-27 19:22:38 +0530

    added API service files for house and character controller

---
+++ PowerPOP/Characters/Service/CharacterRequestEndPoint.swift
@@ -0,0 +1,9 @@
+//
+//  CharacterRequestEndPoint.swift
+//  PowerPOP
+//
+//  Created by Ashish Personal on 27/09/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
