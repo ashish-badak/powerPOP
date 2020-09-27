2a4879e09449274268b53f6a6fa769b1994d558f
Ashish Badak <ashbadak@gmail.com>
2020-09-27 19:33:37 +0530

    created CharacterRequestManager

--- PowerPOP/Characters/Service/CharacterRequestManager.swift
+++ PowerPOP/Characters/Service/CharacterRequestManager.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+final class CharacterRequestManager {
+    /// - TODO: Return mapped data via completion handler
+    func getCharacters() {
+        let requestBuilder = RequestBuilder(requestEndPoint: CharacterRequestEndPoint.characterList)
+        
+        DefaultNetworkService().request(
+            requestBuilder,
+            validator: DefaultAPIResponseValidator()
+        ) { (result) in
+            switch result {
+            case .success(let data):
+                /// - TODO: Map data to Models
+                if let string = String(data: data, encoding: .utf8) {
+                    print("Result: \(string)")
+                }
+                
+            case .failure(let error):
+                print("Error: \(error.description)")
+            }
+        }
+    }
+    
+    /// - TODO: Return mapped data via completion handler
+    func getCharacter(slug: String) {
+        let requestBuilder = RequestBuilder(requestEndPoint: CharacterRequestEndPoint.character(slug: slug))
+        
+        DefaultNetworkService().request(
+            requestBuilder,
+            validator: DefaultAPIResponseValidator()
+        ) { (result) in
+            switch result {
+            case .success(let data):
+                /// - TODO: Map data to Models
+                if let string = String(data: data, encoding: .utf8) {
+                    print("Result: \(string)")
+                }
+                
+            case .failure(let error):
+                print("Error: \(error.description)")
+            }
+        }
+    }
+}
