e2192101215ae423ea1cc900f45fdb4bb4553700
Ashish Badak <ashbadak@gmail.com>
2020-09-27 17:54:54 +0530

    created End point protocol versions

--- PowerPOP/Network Service/RequestEndPoint.swift
+++ PowerPOP/Network Service/RequestEndPoint.swift
@@ -12,9 +12,29 @@ protocol RequestEndPoint {
     var baseURL: String { get }
     var path: String { get }
     
-    var parameters: [String: AnyObject]? { get }
+    var parameters: Params? { get }
     var headers: HTTPHeaders { get }
     
     var httpMethod: HTTPMethod { get }
-    var encoding: ParameterEncoder { get }
 }
+
+extension RequestEndPoint {
+    var baseURL: String { "https://game-of-thrones-quotes.herokuapp.com" }
+    var headers: HTTPHeaders { [:] }
+}
+
+protocol GetRequestEndPoint: RequestEndPoint { }
+
+extension GetRequestEndPoint {
+    var httpMethod: HTTPMethod { .get }
+}
+
+protocol APIVersioning {
+    var apiVersion: String { get }
+}
+
+extension APIVersioning {
+    var apiVersion: String { "v1" }
+}
+
+typealias VersionedGetAPIRequest = GetRequestEndPoint & APIVersioning
