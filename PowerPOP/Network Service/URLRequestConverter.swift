d7f957d90078911c82ddbd590e987b2484695bf3
Ashish Badak <ashbadak@gmail.com>
2020-09-27 18:31:24 +0530

    created URLRequestConverter

--- PowerPOP/Network Service/URLRequestConverter.swift
+++ PowerPOP/Network Service/URLRequestConverter.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+protocol URLRequestConverter {
+    func urlRequest() -> URLRequest?
+}
+
+struct RequestBuilder: URLRequestConverter {
+    let requestEndPoint: RequestEndPoint
+    
+    func urlRequest() -> URLRequest? {
+        guard let baseURL = URL(string: requestEndPoint.baseURL) else {
+            return nil
+        }
+
+        var urlRequest = URLRequest(
+            url: baseURL.appendingPathComponent(requestEndPoint.path),
+            cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+            timeoutInterval: 10.0
+        )
+        
+        urlRequest.httpMethod = requestEndPoint.httpMethod.rawValue
+        urlRequest.allHTTPHeaderFields = requestEndPoint.headers
+        
+        do {
+            if let encoder = requestEndPoint.encoding {
+                try encoder.encode(urlRequest: &urlRequest, with: requestEndPoint.parameters ?? [:])
+            }
+            return urlRequest
+        } catch( _) {
+            return urlRequest
+        }
+    }
+}
