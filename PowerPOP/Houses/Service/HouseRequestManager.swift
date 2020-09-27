d5a72fd191550c3a9dd964674225e8dced0c7653
Ashish Badak <ashbadak@gmail.com>
2020-09-27 19:29:31 +0530

    created HouseRequestManager

--- PowerPOP/Houses/Service/HouseRequestManager.swift
+++ PowerPOP/Houses/Service/HouseRequestManager.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+final class HouseRequestManager {
+    /// - TODO: Return mapped data via completion handler
+    func getHouses() {
+        let requestBuilder = RequestBuilder(requestEndPoint: HouseRequestEndPoint.houseList)
+        DefaultNetworkService().request(
+            requestBuilder,
+            validator: DefaultAPIResponseValidator()
+        ) { (result) in
+            switch result {
+            case .success(let data):
+                /// - TODO: Map data to Models
+                if let string = String(data: data, encoding: .utf8) {
+                    print("Result: \(string)")
+                }
+                
+            case .failure(let error):
+                print("Error: \(error.description)")
+            }
+        }
+    }
+    
+    /// - TODO: Return mapped data via completion handler
+    func getHouse(slug: String) {
+        let requestBuilder = RequestBuilder(requestEndPoint: HouseRequestEndPoint.house(slug: slug))
+        
+        DefaultNetworkService().request(
+            requestBuilder,
+            validator: DefaultAPIResponseValidator()
+        ) { (result) in
+            switch result {
+            case .success(let data):
+                /// - TODO: Map data to Models
+                if let string = String(data: data, encoding: .utf8) {
+                    print("Result: \(string)")
+                }
+                
+            case .failure(let error):
+                print("Error: \(error.description)")
+            }
+        }
+    }
+}
