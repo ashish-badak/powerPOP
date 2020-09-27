50b1b081c15055ff4567ef1dc5c99a46117d5083
Ashish Badak <ashbadak@gmail.com>
2020-09-27 19:19:53 +0530

    updated APIRequestError

--- PowerPOP/Network Service/APIRequestError.swift
+++ PowerPOP/Network Service/APIRequestError.swift
@@ -10,13 +10,31 @@ import Foundation
 
 enum APIRequestError: Error {
     case missingURL
+    case authenticationError
+    case badRequest
+    case outdated
+    case failed
+    case invalidResponse
+    case noData
 }
 
 extension APIRequestError: CustomStringConvertible {
     var description: String {
         switch self {
         case .missingURL:
-            return "URL is missing or it is invalid"
+            return "URL is missing or it is invalid."
+        case .authenticationError:
+            return "Authentication failure."
+        case .badRequest:
+            return "UhOh! Bad Request."
+        case .outdated:
+            return "The url you requested is outdated."
+        case .failed:
+            return "Network request failed."
+        case .invalidResponse:
+            return "Failed to parse response"
+        case .noData:
+            return "Did not receive Data"
         }
     }
 }
